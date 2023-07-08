6653b479d6197389dc0d20c721ce1609655acffe
Pream Pinbut <momozahara@hotmail.com>
2023-07-09 02:03:49 +0700

    lint src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,39 +6,55 @@ dotenv.config();
 let MY_IP: string;
 
 async function getMyIp() {
-  let response = await fetch("https://cloudflare.com/cdn-cgi/trace?format=json");
+  let response = await fetch(
+    "https://cloudflare.com/cdn-cgi/trace?format=json",
+  );
 
   let data = await response.text();
 
-  let obj = data.trim().split("\n").reduce((acc: { [key: string]: string }, curr) => {
-    let [key, value] = curr.split('=');
-    acc[key] = value;
-    return acc;
-  }, {});
+  let obj = data
+    .trim()
+    .split("\n")
+    .reduce((acc: { [key: string]: string }, curr) => {
+      let [key, value] = curr.split("=");
+      acc[key] = value;
+      return acc;
+      // eslint-disable-next-line object-curly-newline
+    }, {});
 
   return obj["ip"];
 }
 
-async function updateDns(type: string, id: string, name: string, content: string, proxied: boolean = true) {
+async function updateDns(
+  type: string,
+  id: string,
+  name: string,
+  content: string,
+  proxied: boolean = true,
+) {
   let headersList = {
-    "Authorization": `Bearer ${process.env.API_TOKEN as string}`,
-    "Content-Type": "application/json"
-  }
+    Authorization: `Bearer ${process.env.API_TOKEN as string}`,
+    "Content-Type": "application/json",
+  };
 
   let bodyContent = JSON.stringify({
-    "type": type,
-    "content": content,
-    "name": name,
-    "ttl": 1,
-    "proxied": proxied
+    type: type,
+    content: content,
+    name: name,
+    ttl: 1,
+    proxied: proxied,
   });
 
   let response = await fetch(
-  `https://api.cloudflare.com/client/v4/zones/${process.env.ZONE_ID as string}/dns_records/${id}`, {
-    method: "PUT",
-    body: bodyContent,
-    headers: headersList
-  });
+    `https://api.cloudflare.com/client/v4/zones/${
+      process.env.ZONE_ID as string
+    }/dns_records/${id}`,
+    {
+      method: "PUT",
+      body: bodyContent,
+      headers: headersList,
+    },
+  );
 
   console.log(await response.text());
 }
@@ -47,21 +63,24 @@ async function firstPoll() {
   MY_IP = await getMyIp();
   record.forEach((item) => {
     updateDns(item.type, item.id, item.cname, MY_IP, item.proxy);
-  })
+  });
 }
 
 firstPoll();
 
-setInterval(async () => {
-  try {
-    let new_ip = await getMyIp();
-    if (MY_IP != new_ip) {
-      MY_IP = new_ip;
-      record.forEach((item) => {
-        updateDns(item.type, item.id, item.cname, MY_IP, item.proxy);
-      })
+setInterval(
+  async () => {
+    try {
+      let newIp = await getMyIp();
+      if (MY_IP !== newIp) {
+        MY_IP = newIp;
+        record.forEach((item) => {
+          updateDns(item.type, item.id, item.cname, MY_IP, item.proxy);
+        });
+      }
+    } catch (e) {
+      console.error(e);
     }
-  } catch (e) {
-    console.error(e);
-  }
-}, 1000 * 60 * 60 * 1);
+  },
+  1000 * 60 * 60 * 1,
+);
